8cbe3681765a9b74ed4d2213e5f722078e4bab16
Geravant <igor.gera.gerasim@gmail.com>
2019-06-04 21:38:11 +0300

    Point implemented, compareTo tests partially implemented, build.gradle modified

--- src/test/java/TestPoint.java
+++ src/test/java/TestPoint.java
@@ -1,8 +1,52 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TestPoint {
 
-    public void testPoin() {
-        Assert.assertTrue(false);
+    @Test
+    public void testCompareToReturns0ForEqualPoints() {
+        //Arrange
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(0,0);
+        int expected = 0;
+        //Act
+        int actual = point1.compareTo(point2);
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCompareToReturns0ForItself() {
+        //Arrange
+        Point point1 = new Point(0,0);
+        int expected = 0;
+        //Act
+        int actual = point1.compareTo(point1);
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCompareToReturns1ForLesserYPoint() {
+        //Arrange
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(0,-1);
+        int expected = 1;
+        //Act
+        int actual = point1.compareTo(point2);
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCompareToReturnsNegative1ForGreaterYPoint() {
+        //Arrange
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(0,1);
+        int expected = -1;
+        //Act
+        int actual = point1.compareTo(point2);
+        //Assert
+        Assert.assertEquals(expected, actual);
     }
 }
