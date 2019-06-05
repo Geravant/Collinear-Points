1c2a08d760c84f4bdf2cec72b29a93c4831aef17
Geravant <igor.gera.gerasim@gmail.com>
2019-06-05 22:33:04 +0300

    Point.slopeTo fixed

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -52,7 +52,7 @@ public class Point implements Comparable<Point> {
         if (this.compareTo(that) == 0) return Double.NEGATIVE_INFINITY;
         else if (this.x == that.x) return Double.POSITIVE_INFINITY;
         else if (this.y == that.y) return +0.0;
-        else return (that.y - this.y)/(that.x - that.x);
+        else return (that.y - this.y)/(that.x - this.x);
     }
 
     /**
